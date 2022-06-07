1cfd046b07b2fb8b0d0390919dc72194a707f4a6
rickie maweu <rickiemaweu@gmail.com>
2022-06-08 01:48:23 +0300

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,35 @@ function ternaryCheckCity(){
 
 function switchOnCharmFromTip(){
   // Write your code here!
+}function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if(feet <= 400){
+    return "This one is on me!";
+  }
+  else if(feet >= 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(feet > 2500){
+    return "No can do.";
+  }
 }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+}
+
+function switchOnCharmFromTip(generous){
+  // Write your code here!
+  switch (generous) {
+    case "generous":
+       return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.'
+  
+    default:
+      return "Bye.";
+  }
+}
+switchOnCharmFromTip("generous")
